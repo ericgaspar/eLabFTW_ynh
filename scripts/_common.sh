52de47012fb147ab224b15b1bb15851194be21d3
ericgaspar <junk.eg@free.fr>
2021-06-13 08:19:07 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,12 +7,12 @@
 
 nodejs_version=14
 
-YNH_PHP_VERSION="7.3"
+YNH_PHP_VERSION="8.0"
 
 # dependencies used by the app
 extra_php_dependencies=""
 
-YNH_COMPOSER_VERSION="1.10.17"
+YNH_COMPOSER_VERSION="2.1.1"
 
 #=================================================
 # PERSONAL HELPERS
